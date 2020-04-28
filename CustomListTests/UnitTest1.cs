3c6ecb11197c93af249957b58ca837d99ba73b5b
Anthony Durham <durhamusa1313@gmail.com>
2020-04-28 13:57:06 -0500

    complety have a full understanding of test methods thanks to nevin

--- CustomListTests/UnitTest1.cs
+++ CustomListTests/UnitTest1.cs
@@ -40,12 +40,44 @@ namespace CustomListTests
             // assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_CapacityProperty()
+        {
+
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 10;
+            int actual;
+
+
+
+            //act
+            testList.Add(itemToAdd);
+            actual = Capacity[];
+
+
+            //asert
+            Assert.AreEqual(expected, actual);
+
+
+        }
         
-        // what happens if you add multiple things (or add to a CustomList that already has some values)?
+       
+
+
+
+
+
+
+
+
+
+            // what happens if you add multiple things (or add to a CustomList that already has some values)?
             // what happens to the last-added item?
             // what happens to the Count?
 
-        // what happens if you add more items than the initial Capacity of the CustomList?
+            // what happens if you add more items than the initial Capacity of the CustomList?
 
-    }
+        }
 }
--- Sandbox/CustomList.cs
+++ Sandbox/CustomList.cs
@@ -11,16 +11,27 @@ namespace Sandbox
         // member variables (HAS A)
         private T[] items;
 
+
         // constructor (SPAWNER)
         public CustomList()
         {
             items = new T[4];
+            
         }
+        public CustomList(int length)
+        {
+            items = new T[length];
+
+        }
+         
 
         // member methods (CAN DO)
         public void Add(T item)
         {
-
+            T[int index]; a
+;
+            if (index < 0 && index >= item.Length)
+                Console.Read IndexOuttOfRange;
         }
     }
 }
